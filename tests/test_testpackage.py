cc50972bb84b7cac010f9afc649cf52d473794b5
Daniel Kiss <kissdanieldezso@gmail.com>
2024-01-24 14:45:12 +0100

    Foobar added.

--- tests/test_testpackage.py
+++ tests/test_testpackage.py
@@ -2,20 +2,5 @@
 
 """Tests for `testpackage` package."""
 
-
-import unittest
-
-from testpackage import testpackage
-
-
-class TestTestpackage(unittest.TestCase):
-    """Tests for `testpackage` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+def foobar():
+    print('Hello')
